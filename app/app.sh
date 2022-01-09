f4442ea419f3e8eae83cc326f75d3b75fe685771
Hich <contact@hitch.fr>
2022-01-09 10:07:01 +0100

    comment out the app loader

--- app/app.sh
+++ app/app.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+# Desiccant by Eachtime https://github.com/eachtime
+# Version : 0.1
+# website: https://desiccant.fr
+#
+# This script is licensed under The GNU AFFERO GENERAL PUBLIC LICENSE.
+# Please read the LICENSE file in the project root for more details 
+
+# Bash failsafe
+set -f # disable globbing
+set -e # exit on script fail
+set -E # ERR trap not fire in certain scenarios with -e only
+set -u # exit on var error
+# set -x # print commands before exec (debug)
+set -o pipefail
+
+# get the app sub directory relative to this file path
 APP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )";
 
-export DESICCANT_PWD=${APP%/*};
+# create and export the read only desiccant working directory variable
+readonly DESICCANT_PWD=${APP%/*};
+export DESICCANT_PWD;
+
+# create and export the JSON Processor executable variable
 export DESICCANT_JQ="$DESICCANT_PWD/libs/jq-linux64";
 
+# load desiccant modules ( temporary organization )
 FUNCTIONS=$DESICCANT_PWD/functions;
 
 source $FUNCTIONS/utils.sh;
@@ -15,5 +35,6 @@ source $FUNCTIONS/openssl.sh;
 source $FUNCTIONS/dehydrated.sh;
 source $FUNCTIONS/sync.sh;
 
+# unset temporary variables
 unset -v FUNCTIONS;
 unset -v APP;

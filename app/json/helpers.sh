5f25d302ca3909a9070dba911d5683868d486af7
Hich <contact@hitch.fr>
2022-01-10 16:34:25 +0100

    create the keys json helper

--- app/json/helpers.sh
+++ app/json/helpers.sh
@@ -7,3 +7,19 @@ function value(){
   local output=$($DESICCANT_JQ ."$args" $configuration_file);
   echo $output | tr -d '[],"';
 }
+
+# list the keys of the json at the given path ${1}, if
+# two params are given list the #keys of the json
+# object ${1} in the json at the path ${2}
+function keys(){
+  if [[ -z ${2+x} ]]
+  then
+    local configuration_file="$1";
+    local args="";
+  else
+    local configuration_file="$2";
+    local args=".$1 |";
+    [[ ${args::1} =~ ^[0-9]$ ]] && args=$( printf '"%s"' $args );
+  fi
+  $DESICCANT_JQ "${args} keys_unsorted" $configuration_file | tr -d '[],"';
+}

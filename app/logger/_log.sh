84977d21cadfd8e768df8a05c04164024b2a56e5
hich <hicham@hitch.fr>
2022-01-12 10:59:21 +0100

    add the check_log_level function

--- app/logger/_log.sh
+++ app/logger/_log.sh
@@ -20,3 +20,25 @@ logger_init(){
   local logfile=$( path $logdir/$filename );
   touch $logfile;
 }
+
+check_log_level(){
+  local message="${@}";
+  local msg_lvl=0;
+  local app_lvl=$( app logger.level );
+
+  if [[ "${message}" == "<%"*"%>"* ]]
+  then
+    msg_lvl=${message%\%\>*}
+    msg_lvl=${msg_lvl#\<\%*level*}
+    msg_lvl=$( trim $msg_lvl );
+    [[ $msg_lvl =~ ^[0-9]+$ ]] || msg_lvl=0; 
+
+    message=${message#\<\%*\%\>};
+    message=$( trim $message );
+  fi
+
+  if (( $msg_lvl <= $app_lvl ))
+  then
+    echo "$message";
+  fi
+}

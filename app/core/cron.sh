e26b569f7b68eec18b7f58b4a0e3016d12f03849
hich <hicham@hitch.fr>
2022-01-13 14:41:29 +0100

    create cron_min function

--- app/core/cron.sh
+++ app/core/cron.sh
@@ -17,3 +17,31 @@ function cron_sanitize() {
 
   echo "$human_freq";
 }
+
+# extract the minutes part of any given
+# human readable cron frequency ${1}
+# return 0 if this part is absent
+function cron_min() {
+  local str="${1}";
+  local min="null";
+  str=$( substitute '@' '' "${str}" );
+
+  if [[ $str =~ ^[0-9]+\:[0-9]+$ ]]
+  then
+    parts=($(explode ":" "${str}"));
+    min="${parts[1]}";
+  elif [[ $str =~ ^[0-9]+h[0-9]+$ ]]
+  then
+    parts=($(explode "h" "${str}"));
+    min="${parts[1]}";
+  fi
+
+  if [[ $min != "null" ]]
+  then
+    # base10 to base10 convertion
+    # auto remove heading zeros
+    min=$((10#$min));
+    [[ "$min" -gt "59" ]] && min=0;
+  fi
+  echo $min;
+}

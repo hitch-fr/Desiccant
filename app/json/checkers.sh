71cc5f8ad0c2dc05a2161ba9c77def561a7ee124
hich <hicham@hitch.fr>
2022-01-13 15:26:15 +0100

    some thought on useful conf checking functions

---
+++ app/json/checkers.sh
@@ -0,0 +1,16 @@
+########################
+# certs configurations #
+########################
+
+# check config files presence
+# check fqdn values
+# check hooks configurations
+
+
+########################
+# hosts configurations #
+########################
+
+# check the email configuration
+# check the cron template presence
+# check the cron frequency

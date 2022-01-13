13aba5bf2d846508cd137c995fc45c699c47bce1
hich <hicham@hitch.fr>
2022-01-13 07:55:33 +0100

    create the 'is_null' utility function

--- app/utils.sh
+++ app/utils.sh
@@ -1,4 +1,4 @@
-# return true if any given string
+# return zero if any given string
 # ${1} is equal to true or yes
 # and return false otherwise
 function is() {
@@ -7,5 +7,17 @@ function is() {
     return 1;
   else
     return 0;
+  fi
+}
+
+# return zero if the ?given variable is
+# not set, contains a null string or
+# is set to the following : "null"
+function is_null() {
+  if [[ -z "${1+x}" ]] || [[ "${1}" == "null" ]]
+  then
+    return 0;
+  else
+    return 1;
   fi
 }

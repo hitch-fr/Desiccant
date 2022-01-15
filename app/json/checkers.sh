41ce0c158dd85d39fd6dd0559c4c1e55c7a86135
hich <hicham@hitch.fr>
2022-01-15 13:46:08 +0100

    improve json checkers

--- app/json/checkers.sh
+++ app/json/checkers.sh
@@ -1,3 +1,49 @@
+function conf_exists(){
+  local conf="${1}";
+  local filename=$( basename $conf );
+
+  if is_file $conf
+  then
+    info "<% level 1 %> the $filename configuration file exists";
+    return 0;
+  else
+    error "the $filename configuration file was not found";
+    info "<% level 1 %> file path : $conf";
+    return 1;
+  fi
+}
+
+function json_is_valid(){
+  local conf="${1}";
+  local filename=$( basename $conf );
+
+  if $DESICCANT_JQ 'keys' $conf &> /dev/null
+  then
+    info "<% level 2 %> the file $filename is a valid json";
+    return 0;
+  else
+    error "the file $filename is not a valid json";
+    info "<% level 1 %> file path : $conf";
+    return 1;
+  fi
+}
+
+function json_is_not_empty(){
+  local conf="${1}";
+  local filename=$( basename $conf );
+
+  local keys=$( keys $conf );
+  if [[ $keys != "" ]]
+  then
+    info "<% level 3 %> the file $filename is not empty";
+    return 0;
+  else
+    error "$filename should not be empty";
+    info "<% level 2 %> file path : $conf";
+    return 1;
+  fi
+}
+
 ##############
 # app checks #
 ##############
@@ -10,37 +56,10 @@
 # hosts configuration #
 #######################
 
-# check hosts.json presence
-function conf_exists(){
-  local conf="${1}";
+# ✓ check hosts.json presence
+# ✓ jq can read the file
+# ✓ hosts conf have at least one host
 
-  info "<% level 4 %> checking existence of $conf";
-  if is_file $conf
-  then
-    info "<% level 3 %> the configuration file exists";
-    return 0;
-  else
-    error "the configuration file was not found";
-    return 1;
-  fi
-}
-
-# jq can read the file
-function json_is_valid(){
-  local conf="${1}";
-
-  info "<% level 4 %> checking json $conf";
-
-  if $DESICCANT_JQ 'keys' $conf &> /dev/null
-  then
-    info "<% level 3 %> the json file is valid";
-    return 0;
-  else
-    error "invalid json file";
-    return 1;
-  fi
-}
-# hosts conf have at least one host
 # maybe check for unknown keys?
 
 ###########################
@@ -60,11 +79,15 @@ function json_is_valid(){
 # check hooks configurations
 
 function config_is_valid() {
+  log_header "Configuration check";
   local servers_conf=$( app hosts );
   servers_conf=$( path "$servers_conf" );
+  info "<% level 3 %> checking hosts configuration";
+  info "<% level 5 %> $servers_conf";
 
   conf_exists $servers_conf && \
   json_is_valid $servers_conf && \
+  json_is_not_empty $servers_conf && \
   info "configuration ok" && \
   return 0;
 }

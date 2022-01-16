a6b3feb2285573289bc7d0f33b8eb52da51817b5
hich <hicham@hitch.fr>
2022-01-16 17:13:25 +0100

    refactor checkers

--- app/json/checkers.sh
+++ app/json/checkers.sh
@@ -59,29 +59,46 @@ function json_is_not_empty(){
 # ✓ check hosts.json presence
 # ✓ jq can read the file
 # ✓ hosts conf have at least one host
-# hosts has required keys
+# ✓ hosts conf have required keys
+# maybe check for unknown keys?
+function hosts_check(){
+  local servers_conf=$( app hosts );
+  servers_conf=$( path "$servers_conf" );
+  info "<% level 3 %> checking hosts configuration";
+  info "<% level 5 %> $servers_conf";
 
-function check_hosts_requirements(){
+  conf_exists $servers_conf && \
+  json_is_valid $servers_conf && \
+  json_is_not_empty $servers_conf && \
+  check_each_host_requirements $servers_conf && \
+  return 0;
+}
+
+function check_each_host_requirements(){
   local conf="${1}";
   local hosts=$( keys $conf );
 
   for host in $hosts
   do
-    local name_is_null_msg="the value $host.name should be the name printed by the hostname command on the computer in which you intend to execute it";
-    local certs_is_null_msg="the host $host should have a field 'certificates' that reference your certificates configuration files in an array";
-
-    host_has_field $host 'name' "$name_is_null_msg";
-    host_has_field $host 'certificates' "$certs_is_null_msg";
-
-    host_field_type $conf $host 'name' 'string';
-    host_field_type $conf $host 'certificates' 'array';
+    check_host_requirements $host $conf;
   done
 }
-# maybe check for unknown keys?
 
 ###########################
 # each host configuration #
 ###########################
+
+function check_host_requirements(){
+  local host="${1}" conf="${2}";
+  local name_is_null_msg="the value $host.name should be the name printed by the hostname command on the computer in which you intend to execute it";
+  local certs_is_null_msg="the host $host should have a field 'certificates' that reference your certificates configuration files in an array";
+
+  host_has_field $host 'name' "$name_is_null_msg";
+  host_has_field $host 'certificates' "$certs_is_null_msg";
+  check_field_type $conf $host 'name' 'string';
+  check_field_type $conf $host 'certificates' 'array';
+  check_certificates $conf $host;
+}
 
 function host_has_field(){
   local host="${1}" field="${2}";
@@ -108,7 +125,7 @@ function host_has_field(){
   fi
 }
 
-function host_field_type(){
+function check_field_type(){
   local conf="${1}" host="${2}" field="${3}" datatype="${4}";
 
   datatype=$( echo "${datatype}" | tr '[:upper:]' '[:lower:]' );
@@ -132,15 +149,15 @@ function host_field_type(){
   fi
 }
 
-# check the email configuration if email enabled
+# check the common reporter configuration if enabled
 # check the cron templates presence if cron enabled
 # check the cron frequency if cron enabled
 
 ########################
 # certs configurations #
 ########################
-function check_certificate_requirement(){
-  local host="${1}" cert_conf="${2}";
+function check_certificates(){
+  local conf="${1}" host="${2}";
   local disabled=$( server $host.disabled );
 
 }
@@ -148,18 +165,6 @@ function check_certificate_requirement(){
 # check fqdn values
 # check hooks configurations
 
-function hosts_check(){
-  local servers_conf=$( app hosts );
-  servers_conf=$( path "$servers_conf" );
-  info "<% level 3 %> checking hosts configuration";
-  info "<% level 5 %> $servers_conf";
-
-  conf_exists $servers_conf && \
-  json_is_valid $servers_conf && \
-  json_is_not_empty $servers_conf && \
-  check_hosts_requirements $servers_conf && \
-  return 0;
-}
 
 function config_is_valid() {
   log_header "Configuration check";

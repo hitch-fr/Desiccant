00da1fb73bdb8de3ab60d7353c2efc87990a21e7
hich <hicham@hitch.fr>
2022-01-18 10:24:53 +0100

    update app loader

--- app/load.sh
+++ app/load.sh
@@ -18,9 +18,6 @@ export DESICCANT_PWD;
 # create and export the JSON Processor executable variable
 export DESICCANT_JQ="$DESICCANT_PWD/libs/jq-linux64";
 
-# load desiccant modules ( temporary organization )
-FUNCTIONS=$DESICCANT_PWD/functions;
-
 source $APP/core/utils.sh;
 source $APP/json/helpers.sh;
 source $APP/json/checkers.sh;
@@ -28,16 +25,13 @@ source $APP/log/functions.sh;
 source $APP/cron/common.sh;
 source $APP/send/reports.sh;
 
-source $FUNCTIONS/openssl.sh;
 source $APP/core/openssl.sh;
 source $APP/core/dehydrated.sh;
 
 # remote functions
 source $APP/core/remote.sh;
 source $APP/cron/remote.sh;
-source $FUNCTIONS/sync.sh;
 source $APP/core/sync.sh;
 
 # unset temporary variables
-unset -v FUNCTIONS;
 unset -v APP;
